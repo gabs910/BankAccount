75598046b1f5f192644e5d9acaa3b25729eaa185
Gabs910 <gabham910@hotmail.com>
2016-08-23 16:39:39 -0400

    New Account class for Assignment 6

--- Account.java
+++ Account.java
@@ -4,50 +4,85 @@ public class Account {
 	private int accountId;
 	String description;
 	double balance;
+	private static int idCounter = 1000; 
 
 	String print() {
-		return description + '\n' + "ID: " + getAccountId() + " " + "Balance: " + balance;
-	}
+		System.out.println( "Account: " + getAccountId() + " " + "balance is $" + balance + '\n');
+		return  description + "Account: " + getAccountId() + " " + "balance is $" + balance + '\n';
 
+	}
+	private void setAccountId(){
+		this.setAccountId(idCounter);
+		idCounter++;
+	}
+	// deposit method adds to balance when called
 	double deposit(double amount) {
 		balance += amount;
-		System.out.println("You deposited:" + amount);
 		return balance;
 	}
 
-	double withdrawal(double amount) {
+	// withdrawal method subtracts from balance when call unless condition is
+	// not met
+	double withdraw(double amount) {
 		if (balance < amount) {
-			System.out.println("Insufficient funds!");
+			System.out.println("Insufficient funds!" + '\n');
 		} else {
 			balance -= amount;
-			System.out.println("You withdrew: " + amount);
 		}
 		return balance;
 	}
 
-	double getBalance() {
-		return balance;
-	}
-
-	double toTransfer(Account other, double amount) {
+	double transferFrom(Account other, double amount) {
 		if (balance < amount) {
 			System.out.println("Insufficient funds to make requested transer.");
 		} else {
 			other.deposit(amount);
-			System.out.println("You transferred: " + amount + " to your savings account");
 			balance -= amount;
 
 		}
 		return balance;
 
 	}
-
+// getter for accountId
 	public int getAccountId() {
 		return accountId;
 	}
-
-	public void setAccountId(int accountId) {
+// setter for accountId
+	private void setAccountId(int accountId) {
 		this.accountId = accountId;
 	}
+//getter for balance
+	public double getBalance() {
+		return balance;
+	}
+//setter for balance can only be used within class
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+//getter method getDescription
+	public String getDescription() {
+		return description;
+	}
+//setter method description
+	public void setDescription(String aDescription) {
+		description = aDescription;
+	}
 
+	public Account() {
+		this.setAccountId();
+		this.balance = 0;
+
+	}
+
+	public Account(String aDescription) {
+		this.setAccountId();
+		description = aDescription;
+		this.balance = 0;
+		System.out.println();
+	}
+	public Account(int id, String aDescription) {
+		aDescription = description;
+		accountId = id;
+		this.balance = 0;
+	}
 }
